0cafdb85fcb96a3b69b112bb58dd2d18c36a909f
Dain Blodorn Kim <dmbk@dains-mbp.lan>
2018-10-02 19:16:52 -0700

    more updates

--- src/state/reducers/content.js
+++ src/state/reducers/content.js
@@ -1,7 +1,11 @@
 import config from './../../config.json'
+import theme from './../../styles/theme.json'
 
 const content = {
   ...config,
+  style_theme: {
+    ...theme
+  },
   menu: [
     {
       slug: 'link',
